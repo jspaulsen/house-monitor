d3cb8b44f64c5ad4d164e6da774653308b369349
jpaulsen <jpaulsen.dev@gmail.com>
2022-07-24 16:12:07 -0700

    Support multi-arch builds
    Signed-off-by: jpaulsen <jpaulsen.dev@gmail.com>

--- house_monitor_sub/publish.sh
+++ house_monitor_sub/publish.sh
@@ -4,5 +4,11 @@ set -ex
 VERSION=$(cat VERSION)
 source docker.env
 
-source build.sh
-docker push "${DOCKER_REPO}/${DOCKER_IMAGE}:${VERSION}"
+
+docker buildx \
+    build \
+    --platform linux/amd64,linux/arm64 \
+    --target ${TARGET} \
+    -t "${DOCKER_REPO}/${DOCKER_IMAGE}:${VERSION}" \
+    --push \
+    .
